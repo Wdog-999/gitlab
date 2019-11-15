d996288600db279a9e0c547c60f3a56c55342a4e
Wdog-999 <wdog_999@yahoo.com>
2019-11-15 14:48:03 -0500

    The commit is dangerous

--- hello.java
+++ hello.java
@@ -2,6 +2,7 @@ public class Hello
 {
 public static void main( String argv[] )
 	{
+		//The comment has now taken the mayor of the town hostage! Oh, the humanity!
 		String name = "World";
 		if ( argv.length != 0 )
 		{
